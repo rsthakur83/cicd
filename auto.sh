cd61b3fd6629bdb6103319ad38a7413326cd04ae
rsthakur83 <rsthakur83@yahoo.com>
2019-04-22 11:25:33 +0100

    newapp

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sg1=`aws ec2 describe-security-groups --filters Name=tag:Name,Values=asg1 --query 'SecurityGroups[].GroupId' --region us-east-1 --output text`
-sg2=`aws ec2 describe-security-groups --filters Name=tag:Name,Values=asg2 --query 'SecurityGroups[].GroupId' --region us-east-1 --output text`
+sg1=`aws ec2 describe-security-groups --filters Name=tag:Name,Values=sg1 --query 'SecurityGroups[].GroupId' --region us-east-1 --output text`
+sg2=`aws ec2 describe-security-groups --filters Name=tag:Name,Values=sg2 --query 'SecurityGroups[].GroupId' --region us-east-1 --output text`
 vpcid=`aws ec2 describe-vpcs --filters Name=tag:Name,Values=myappvpc --query 'Vpcs[].[VpcId]' --region us-east-1 --output text`
 
 sed -i -e "s/sg1/$sg1/g" fis/sg.tf
